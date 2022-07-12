b94ca4fbfbe6ff327e29259944c75196bb795558
deathsmilestome <vanekt35@mail.ru>
2022-07-12 21:20:39 +0300

    + tips in comments

--- Direct_control.py
+++ Direct_control.py
@@ -3,6 +3,10 @@ from HandTrackModule import HandDet
 import math
 import pyautogui as pg
 
+# 1920x1080 screen
+# 640x480 cam
+# 512x288 work space
+
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -19,16 +23,15 @@ def main():
             x8, y8 = lms_list[8][1], lms_list[8][2]
             x12,y12 = lms_list[12][1], lms_list[12][2]
             x16,y16 = lms_list[16][1], lms_list[16][2]
-            x20,y20 = lms_list[20][1], lms_list[20][2]
             if lms_list[4][2] < lms_list[8][2]:
                 break
-            elif x5 in range (64,576) and y5 in range(96,388) and math.hypot(x5-x4, y5-y4) > 20 :
-                pg.moveTo(1920 - int((x5 - 64) * 3.75), int((y5 - 96) * 3.75))
+            elif x5 in range (64,576) and y5 in range(96,388) and math.hypot(x5-x4, y5-y4) > 20:  # change this range for more work space
+                pg.moveTo(1920 - int((x5 - 64) * 3.75), int((y5 - 96) * 3.75))  # 512x288 * 3,75 = 1920x1080
             elif math.hypot(x12-x8, y12-y8) < 20:
                 pg.click()
             elif math.hypot(x12-x16, y12-y16) < 20:
                 pg.rightClick()
-        cv2.rectangle(img, (64, 96), (576, 388), (255, 0, 255), 1)
+        cv2.rectangle(img, (64, 96), (576, 388), (255, 0, 255), 1)  # drawing work space
         cv2.circle(img, (x_center,y_center), 0, (255, 0, 255), 2)
         cv2.imshow("Image", cv2.flip(img, 1))
         cv2.waitKey(1)
--- controller.py
+++ controller.py
@@ -3,6 +3,9 @@ from HandTrackModule import HandDet
 import math
 import pyautogui as pg
 
+# 1920x1080 screen
+# 640x480 cam
+
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -19,14 +22,14 @@ def main():
             x12,y12 = lms_list[12][1], lms_list[12][2]
             if lms_list[4][2] > 450:
                 break
-            elif x8 in range (240,400) and y8 in range(160,320) and math.hypot(x8-x4, y8-y4) > 20 :
+            elif x8 in range (240,400) and y8 in range(160,320) and math.hypot(x8-x4, y8-y4) > 20: # change this range for more work space
                 x_dif = x8 - x_center
                 y_dif = y8 - y_center
                 cv2.line(img, (x_center, y_center), (x8,y8), (255, 0, 255), 1)
                 pg.moveTo(pg.position().x - x_dif, pg.position().y + y_dif)
             elif math.hypot(x8-x12, y8-y12) < 20:
                 pg.click()
-        cv2.rectangle(img, (240, 160), (400, 320), (255, 0, 255), 1)
+        cv2.rectangle(img, (240, 160), (400, 320), (255, 0, 255), 1)  # drawing work space
         cv2.circle(img, (x_center,y_center), 0, (255, 0, 255), 2)
         cv2.line(img, (0, 450), (640, 450), (255, 0, 255), 1)
         cv2.imshow("Image", cv2.flip(img, 1))
